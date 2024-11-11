3db7480ab9afef1388ca4bec40649856852f0824
deniz <deniz@r1oaz.ru>
2024-11-11 22:17:32 +0300

    Fixed the text splitter

--- stream_text.py
+++ stream_text.py
@@ -3,40 +3,26 @@ from pydub import AudioSegment as seg
 import numpy as np
 
 def smartsplit(text):
-    # each chunk can be at most 64 bytes
     chunks = []
     current_chunk = ""
-    sep_idx = -1  # what if the accumulated text is too long, we need to quickly remember the last separator
-    space_idx = -1  # space is the last resort if no way to split
-    for i, char in enumerate(text):
-        if char in [",", ";", ".", "!", "?"]:
-            sep_idx = i
-        if char == " ":
-            space_idx = i
-        if char == "\n":
+    # split the whole text by lines
+    lines: list[str] = text.split("\n")
+    for line in lines:
+        words = line.split()
+        for word in words:
+            hypothesis = current_chunk + " " + word
+            if len(hypothesis.encode("UTF-8")) >= 64:
+                chunks.append(current_chunk)
+                current_chunk = word
+            else:
+                current_chunk = hypothesis
+        if current_chunk:  # if a line has words
             chunks.append(current_chunk)
             current_chunk = ""
-            space_idx = -1
-            sep_idx = -1
-        # 64 bytes, not 64 characters
-        if len(current_chunk.encode("utf-8")) >= 64:
-            if sep_idx > -1:  # if we have a separator, split there
-                chunks.append(current_chunk[:sep_idx])
-                current_chunk = current_chunk[sep_idx:].lstrip()  # dont care about the space
-                sep_idx = -1
-                space_idx = -1  # we fresh start from the new chunk
-            elif space_idx != -1:
-                chunks.append(current_chunk[:space_idx])
-                current_chunk = current_chunk[space_idx:]
-                sep_idx = -1
-                space_idx = -1
-            else:  # ok, lets corrupt the data
-                chunks.append(current_chunk)
-                current_chunk = ""
-
-        current_chunk += char
-    chunks.append(current_chunk)
+    if current_chunk:  # if the last line has words
+        chunks.append(current_chunk)
     return chunks
+
 
 def make_wave(text, *args, **kwargs):  # args and kwargs are passed to ggwave.encode
     chunks = smartsplit(text)
